20b43d2d57f57690eb314d238712a21e6ac81702
flackx <brendons.liflands@va.lv>
2023-10-12 10:37:35 +0300

    bruh

---
+++ ClientService/target/classes/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO companies (compName, bio) VALUES ('ABC Corp', 'A global tech company specializing in software development and IT services');
+INSERT INTO companies (compName, bio) VALUES ('XYZ Ltd', 'An e-commerce company known for its wide range of products and fast shipping services');
+INSERT INTO companies (compName, bio) VALUES ('Green Energy Solutions', 'A renewable energy company focused on solar and wind power solutions');
+INSERT INTO companies (compName, bio) VALUES ('HealthTech Innovations', 'A healthcare technology company that develops cutting-edge medical devices and software');
+INSERT INTO companies (compName, bio) VALUES ('Eco-Friendly Foods', 'A sustainable food production company committed to organic farming and eco-friendly practices');
+INSERT INTO companies (compName, bio) VALUES ('FashionHub', 'A popular fashion and apparel retailer with a strong online presence');
+INSERT INTO companies (compName, bio) VALUES ('Swift Logistics', 'A logistics and transportation company offering efficient supply chain solutions');
+INSERT INTO companies (compName, bio) VALUES ('Blue Wave Entertainment', 'A multimedia entertainment company producing movies, music, and TV shows');
+INSERT INTO companies (compName, bio) VALUES ('EcoSolutions Inc.', 'An environmental consulting company specializing in green initiatives and conservation efforts');
+INSERT INTO companies (compName, bio) VALUES ('TechGenius Labs', 'A research and development company focusing on cutting-edge technology and innovation');
+
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('John', 'Doe', 'johndoe', 'johndoe@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Jane', 'Smith', 'janesmith', 'janesmith@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Michael', 'Johnson', 'michaelj', 'michael@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Emily', 'Brown', 'emilyb', 'emily@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('David', 'Wilson', 'davidw', 'david@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Sophia', 'Anderson', 'sophiaa', 'sophia@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Daniel', 'Martinez', 'danm', 'daniel@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Olivia', 'Garcia', 'oliviag', 'olivia@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('William', 'Lopez', 'williaml', 'william@example.com');
+INSERT INTO persons (firstName, surname, userName, email) VALUES ('Emma', 'Davis', 'emmad', 'emma@example.com');
+
+
